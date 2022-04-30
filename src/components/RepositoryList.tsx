7749ede76533305039d6e3c1479c309143f6e152
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2022-04-30 04:18:44 -0400

    Criando Projeto de Repositorios List do github
    Projeto de Api do github

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+
+// https://api.github.com/orgs/rocktseat/repos
+
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+
+ // fetch('https://api.github.com/orgs/rocketseat/repos')
+
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+    
+    useEffect(() => {
+        fetch('https://api.github.com/users/marcoscode404/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    },[]);
+    // executa uma função quando a variavel mudar
+
+
+    return (
+        <>
+            <section className="repository-list">
+                <h1>Lista de Repositorio</h1>
+
+                <ul>
+                    {repositories.map(repository => (
+                        <RepositoryItem key={repository.name} repository={repository} />
+                        
+                    ))}   
+                </ul>
+
+
+                <footer>
+                    🌃🌌☄️ ViniCode - ColabDevs🚀👩‍🚀🧑‍🚀
+                </footer>
+            
+            </section>
+
+           
+        </>
+    )
+}
